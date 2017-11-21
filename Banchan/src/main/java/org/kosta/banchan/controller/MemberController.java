09167c8c767262b7ce93e2f405ae4a6bcc1565ab
kosta <kosta@192.168.0.188>
2017-11-21 13:39:09 +0900

    2017.11.21 13:38 광태:회원가입후 alert 추가

--- Banchan/src/main/java/org/kosta/banchan/controller/MemberController.java
+++ Banchan/src/main/java/org/kosta/banchan/controller/MemberController.java
@@ -74,7 +74,7 @@ public class MemberController {
 	    	//System.out.println("memberVO :"+memberVO);
 	    	memberService.registerMember(memberVO);
 	    	
-	    	return "redirect:member/loginView.do";
+	    	return "redirect:member/registerMember_ok.do";
 	    }
 	    
 	   ///////////////start 위치기반//////////
