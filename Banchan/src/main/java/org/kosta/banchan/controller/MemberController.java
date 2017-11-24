e6c41e29e2a0bcadae5dfce082ded2b9b2e5dc47
kosta <kosta@192.168.0.120>
2017-11-24 17:49:06 +0900

    정훈 완료

--- Banchan/src/main/java/org/kosta/banchan/controller/MemberController.java
+++ Banchan/src/main/java/org/kosta/banchan/controller/MemberController.java
@@ -16,6 +16,7 @@ import org.kosta.banchan.model.vo.FoodVO;
 import org.kosta.banchan.model.vo.MemberVO;
 import org.kosta.banchan.model.vo.PwQnaVO;
 import org.kosta.banchan.model.vo.SellerVO;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
