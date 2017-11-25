3dbff5cab7eac4d0c702201e5f8a157701ee1286
Administrator <Administrator@192.168.0.148>
2017-11-25 18:11:36 +0900

    2017/11/25 18:11 foodsell_detail.jsp feedback.xml,myTradeList.jsp 수정 _윤주

--- Banchan/src/main/java/org/kosta/banchan/model/dao/ReviewDAO.java
+++ Banchan/src/main/java/org/kosta/banchan/model/dao/ReviewDAO.java
@@ -1,11 +1,14 @@
 package org.kosta.banchan.model.dao;
 
+import java.util.List;
+
 import org.kosta.banchan.model.vo.ReviewVO;
 
 public interface ReviewDAO {
 	/////////////start윤주//////////////
 	void reviewRegister(ReviewVO rvo);
 	void updateSellerScore(String memId);
+	List<ReviewVO> getReviewListByFoodSellNo(String foodSellNo);
 	////////////end윤주/////////////////////
 	
 }
